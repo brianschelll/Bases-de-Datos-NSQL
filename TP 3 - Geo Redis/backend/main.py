eab3382abe7d83c565504a8b1f01f3c8e04d2a24
Brian Schell <brianschell19@gmail.com>
2026-04-15 22:36:50 -0300

    Entrega TP 3

--- TP 3 - Geo Redis/backend/main.py
+++ TP 3 - Geo Redis/backend/main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 import redis
 import os
 
-# Inicializamos la API
+
 app = FastAPI(title="API Turismo - Bases de Datos NoSQL")
 
 app.add_middleware(
@@ -15,27 +15,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Conexión a Redis. Toma los datos del docker-compose.yml
+
 redis_host = os.getenv("REDIS_HOST", "redis-db")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 
-# decode_responses=True hace que Redis nos devuelva strings normales en vez de bytes
+
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
-# Modelo de datos que esperamos recibir cuando se agregue un lugar
+
 class Lugar(BaseModel):
     nombre: str
     latitud: float
     longitud: float
-    grupo: str  # Ej: cervecerias, universidades, farmacias
+    grupo: str  
 
 @app.post("/api/lugares", tags=["Lugares"])
 def agregar_lugar(lugar: Lugar):
     try:
-        # Armamos el nombre de la clave en Redis, ej: "geo:universidades"
+        
         clave_redis = f"geo:{lugar.grupo.lower()}"
         
-        # OJO ACÁ: Redis siempre pide primero la Longitud y después la Latitud
+        
         r.geoadd(clave_redis, (lugar.longitud, lugar.latitud, lugar.nombre))
         
         return {
@@ -55,7 +55,7 @@ def buscar_cercanos(latitud: float, longitud: float, grupo: str, radio_km: int =
     try:
         clave_redis = f"geo:{grupo.lower()}"
         
-        # Le pedimos a Redis que busque en el radio indicado y nos traiga la distancia (withdist=True)
+        
         resultados = r.geosearch(
             name=clave_redis,
             longitude=longitud,
@@ -65,7 +65,7 @@ def buscar_cercanos(latitud: float, longitud: float, grupo: str, radio_km: int =
             withdist=True
         )
         
-        # Acomodamos los datos para que el frontend los pueda leer fácil
+        
         lugares_formateados = []
         for lugar in resultados:
             lugares_formateados.append({
@@ -88,16 +88,16 @@ def calcular_distancia(latitud: float, longitud: float, grupo: str, nombre_lugar
     try:
         clave_redis = f"geo:{grupo.lower()}"
         
-        # 1. Agregamos al usuario temporalmente a Redis
+        
         r.geoadd(clave_redis, (longitud, latitud, "UsuarioTemp"))
         
-        # 2. Calculamos la distancia exacta con el lugar elegido
+        
         distancia = r.geodist(clave_redis, "UsuarioTemp", nombre_lugar, unit='km')
         
-        # 3. Borramos al usuario temporal con ZREM (porque Geo es un Sorted Set por debajo)
+        
         r.zrem(clave_redis, "UsuarioTemp")
         
-        # Si la distancia es None, significa que escribimos mal el nombre del lugar
+        
         if distancia is None:
             raise HTTPException(status_code=404, detail="No se encontró el lugar indicado en la base de datos")
             
